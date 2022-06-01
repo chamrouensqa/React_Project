6ae88a882299b3b7ea34688b2d3e0ae3b35b7382
chamrouensann <96223936+chamrouensqa@users.noreply.github.com>
2022-06-01 10:23:46 +0700

    Using react component

--- react-refresher-01-react-starting-project/src/App.js
+++ react-refresher-01-react-starting-project/src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
-const App = () => {
-  return <h1>A React App!</h1>;
-};
+// const App = () => {
+//   return <h1>My name is Sann chamrouen!</h1>;
+// };
+
+//Using React Component
+class App extends React.Component{
+  render(){
+    return <h1 title='I love you'>Programming language</h1>
+  }
+}
 
 export default App;
